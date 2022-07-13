360419230c7e9efeb6d1052f3032ab9cd3e67c95
sashaholovenko <sasha155s49@gmail.com>
2022-07-13 16:55:36 +0300

    Finished Routers, added Controllers

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
 const router = new Router()
+const deviceRouter = require('./deviceRouter')
+const userRouter = require('./userRouter')
+const brandRouter = require('./brandRouter')
+const typeRouter = require('./typeRouter')
 
-router.use('/user', )
-router.use('/type', )
-router.use('/brand', )
-router.use('/device', )
+
+router.use('/user', userRouter)
+router.use('/type', typeRouter)
+router.use('/brand', brandRouter)
+router.use('/device', deviceRouter)
 
 module.exports = router
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const router = new Router()
+const userControlller = require('../controllers/userController')
 
-router.post('/registration')
-router.post('/login')
-router.get('/auth')
+router.post('/registration', userControlller.registration)
+router.post('/login', userControlller.login)
+router.get('/auth', userControlller.check)
 
 module.exports = router
