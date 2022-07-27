31357c4a65d84fc753ef8ffbb19e25e05ea4879c
sashaholovenko <sasha155s49@gmail.com>
2022-07-27 13:24:25 +0300

    Connecting client and server, registration

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Card, Container, Form, Row} from "react-bootstrap";
 import {NavLink, useLocation} from "react-router-dom";
 import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
+import {login, registration} from "../http/userApi";
 
 const Auth = () => {
     const location = useLocation()
     const isLogin = location.pathname === LOGIN_ROUTE
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const click = async () => {
+        if (isLogin) {
+            const response = await login()
+
+        } else {
+            const response = await registration(email, password)
+            console.log(response)
+        }
+    }
 
     return (
         <Container
@@ -18,10 +32,15 @@ const Auth = () => {
                     <Form.Control
                         className="mt-3"
                         placeholder="Enter your email..."
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <Form.Control
                         className="mt-3"
                         placeholder="Enter your password..."
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
                     />
                     <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
                         {isLogin ? <div>
@@ -34,7 +53,7 @@ const Auth = () => {
                         {/*<div>*/}
                         {/*    Don't have account? <NavLink to={REGISTRATION_ROUTE}>Register!</NavLink>*/}
                         {/*</div>*/}
-                        <Button variant="outline-success" className="mt-3">
+                        <Button variant="outline-success" className="mt-3" onClick={click}>
                             {isLogin ? 'Enter' : 'Registration'}
                         </Button>
 
