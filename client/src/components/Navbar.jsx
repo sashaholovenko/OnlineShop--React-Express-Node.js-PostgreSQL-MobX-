46e9a4934fae62685dbaffcbde08cac8c0ce2671
sashaholovenko <sasha155s49@gmail.com>
2022-07-27 05:40:08 +0300

    BrandBar, Page with devices, Single device page finished

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,33 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {NavLink} from "react-router-dom";
 import {observer} from "mobx-react-lite";
+import {useHistory} from 'react-router-dom'
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const history = useHistory()
     const test = true
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color:'white'}} to={SHOP_ROUTE}>HLVK Shop</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={"outline-light"} style={{marginRight: "5px"}}>Admin Panel</Button>
-                        <Button variant={"outline-light"} onClick={() => user.setIsAuth(false)}>Log Out</Button>
+                        <Button
+                            variant={"outline-light"}
+                            style={{marginRight: "5px"}}
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >Admin Panel
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(LOGIN_ROUTE)}
+                        >Log Out
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: 'white'}}>
